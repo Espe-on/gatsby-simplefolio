35d9867975093391740a610d3a1f95f62b5fe6fe
espe-on <eljoe0@googlemail.com>
2020-04-19 15:14:13 +0100

    neatened layout of data and small spelling corrections

--- src/mock/data.js
+++ src/mock/data.js
@@ -19,10 +19,12 @@ export const heroData = {
 export const aboutData = {
   // img: 'profile.jpg',
   paragraphOne:
-    'I love learning new technologies, I want to make software more accessible for users and encourage people to engage with computers in a deeper way. My role as a software developer and engineer is to empower the people who use my software to achieve their own ends.',
+    'I love learning new technologies, I want to make software more accessible for users and encourage people to engage with computers in a deeper way. ' +
+    'My role as a software developer and engineer is to empower the people who use my software to achieve their own ends.',
   paragraphTwo: 'I am currently on a programming bootcamp run by Softwire called TechSwitch.',
   paragraphThree:
-    'I love to work in FrontEnd web development, I enjoy the challenge of creating, deploying, and improving websites. My favourite technologies at the moment are Typescript, NextJS and SCSS',
+    'I love FrontEnd web development, I enjoy the challenge of creating, deploying, and improving websites. ' +
+    'My favourite technologies at the moment are Typescript, NextJS and SCSS',
   resume: 'https://drive.google.com/open?id=1igEuUM8gOLkmWsASdOjpZZvReFst9sRb', // if no resume, the button will not show up
 };
 
@@ -32,9 +34,10 @@ export const projectsData = [
     id: uuidv1(),
     img: 'testswitchproject.jpg',
     title: 'TestSwitch',
-    info: 'Using NextJS, Monaco, ASP.NET, and PostgreSQL to create an online tech test platform',
+    info:
+      'An online platform to run tech tests, TestSwitch is an ongoing team project by the current cohort of Techswitch, due for final release May 7th.',
     info2:
-      'An online platform to run tech tests, TestSwitch is an ongoing team project by the current cohort of Techswitch, due for final release May 7th.  Technologies Used: NextJS, React, Typescript, SASS, NPM, Git, Github, Trello, Jest, React-Testing Library CircleCI, Heroku',
+      'Technologies Used: NextJS, React, Typescript, Monaco Text Editor, SASS, NPM, PostgreSQL, ASP.net Git, Github, Trello, Jest, React-Testing Library CircleCI, Heroku.',
     url: 'https://testswitch-candidate.herokuapp.com/',
     repo: 'https://github.com/techswitch-learners/testswitch-frontend-candidate', // if no repo, the button will not show up
   },
@@ -43,9 +46,12 @@ export const projectsData = [
     img: 'marsmissionproject.jpg',
     title: 'MarsMission',
     info:
-      'A React based website to educate children about NASA’s Mars Rovers A team of seven of us worked together to make a website from the ground up over the course of a week using NASA’s public APIs.',
+      'A React based website to educate children about NASA’s Mars Rovers. ' +
+      'A team of seven of us worked together to make a website from the ground up over the course of a week using NASA’s public APIs. ' +
+      'For many of us it was our first team software development project, learning how to work effectively in pairs as well as a whole team. ' +
+      'In this project I used Typescript for the first time to create type definitions for the whole team for use in common components. ',
     info2:
-      'For many of us it was our first team software development project, learning how to work effectively in pairs as well as a whole team. In this project I used Typescript for the first time to create type definitions for the whole team for use in common components. Technologies Used: React, Typescript, SASS, Yarn, Git, Github, Trello, Jest, CircleCI, Heroku',
+      'Technologies Used: React, Typescript, SASS, Yarn, Git, Github, Trello, Jest, CircleCI, Heroku.',
     url: 'https://techswitch-mars-mission.herokuapp.com/',
     repo: 'https://github.com/techswitch-learners/mars-mission', // if no repo, the button will not show up
   },
@@ -54,9 +60,11 @@ export const projectsData = [
     img: 'busboardproject.jpg',
     title: 'BusBoard',
     info:
-      'A console based C# app that takes in a Postcode and Returns bus arrival times from the TFL public API.',
-    info2:
-      'One of my first projects to involve using APIs, this program used the TFL API aswell as a the PostcodeIO API. I used to PostcodeIO API to validate postcode and then convert them into LonLat coordinates, which are then passed to the TFL API to look for nearby endpoints with type BusStop and their arrival times. I then formatted that info to display bus arrival times. Technologies Used: C#, .NetCore, TFL API, PostcodeIO',
+      'A console based C# app that takes in a Postcode and Returns bus arrival times from the TFL public API. ' +
+      'One of my first projects to involve using APIs, this program used the TFL API as well as a the PostcodeIO API. ' +
+      'I used to PostcodeIO API to validate postcode and then convert them into LonLat coordinates, which are then passed to the TFL API to look for nearby endpoints with type BusStop and their arrival times. ' +
+      'I then formatted that info to display bus arrival times.',
+    info2: 'Technologies Used: C#, .NetCore, TFL API, PostcodeIO.',
     url: 'https://github.com/Espe-on/busboard-csharp-template',
     repo: 'https://github.com/Espe-on/busboard-csharp-template', // if no repo, the button will not show up
   },
@@ -64,9 +72,10 @@ export const projectsData = [
     id: uuidv1(),
     img: 'personalprofileproject.jpg',
     title: 'Personal ProfilePage',
-    info: 'Developing a Profile Page for the next version of the website',
-    info2:
-      'Using the technologies learnt while on my tech bootcamp I am building a custom personal profile page using NextJS. Technologies Used: NextJs, React, Typescript, SASS, NPM, Git, Github, Zeit',
+    info:
+      'Developing a Profile Page for the next version of this website.' +
+      'Using the technologies learnt while on my tech bootcamp I am building a custom personal profile page using NextJS.',
+    info2: 'Technologies Used: NextJs, React, Typescript, SASS, NPM, Git, Github, Zeit.',
     url: 'https://profile-page-gold.now.sh/',
     repo: 'https://github.com/Espe-on/profile_page', // if no repo, the button will not show up
   },
@@ -75,8 +84,10 @@ export const projectsData = [
     img: 'wpkgproject.jpg',
     title: 'WPKG Software Deployment',
     info:
-      ' Building packages to install software and push software updates to Windows based visual effects and designers. I managed the automatic deployment of software, licenses, and plugins for Adobe suite. This involved writing .xml files to define how the software was to be installed, what consisted as a successful installation, and what indicated an incorrect installation',
-    info2: 'Technologies Used: WPKG, Git, XML, Batch Scripting',
+      ' Building packages to install software and push software updates to Windows based visual effects and designers. ' +
+      'I managed the automatic deployment of software, licenses, and plugins for Adobe suite. ' +
+      'This involved writing .xml files to define how the software was to be installed, what consisted as a successful installation, and what indicated an incorrect installation',
+    info2: 'Technologies Used: WPKG, Git, XML, Batch Scripting.',
     url: 'https://wpkg.org/Main_Page',
     repo: 'https://wpkg.org/Main_Page', // if no repo, the button will not show up
   },

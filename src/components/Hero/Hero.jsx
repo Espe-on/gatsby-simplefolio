3677313cf43418fde046af6085f29be044e65912
espe-on <eljoe0@googlemail.com>
2020-04-19 15:22:33 +0100

    bodge to fix slow loading from data.js

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ import PortfolioContext from '../../context/context';
 
 const Header = () => {
   const { hero } = useContext(PortfolioContext);
-  const { title, name, subtitle, cta } = hero;
+  const { title } = hero;
 
   const [isDesktop, setIsDesktop] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
@@ -25,16 +25,14 @@ const Header = () => {
       <Container>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
-            {title || 'Hi, I am'}{' '}
-            <span className="text-color-main">{name || 'Joseph Crawley'}</span>
-            <br />
-            {subtitle || 'I am a software developer'}
+            {title || 'Hi, I am'} <span className="text-color-main">Joseph Crawley</span>
+            <br />I am a software developer
           </h1>
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
           <p className="hero-cta">
             <a className="cta-btn cta-btn--hero" href="#about">
-              {cta || 'Find out more'}
+              Find out more
             </a>
           </p>
         </Fade>
